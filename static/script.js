d8484fe472c43b6e44191cb163d1d603cd1f8616
trnKhanh <tnk29082003@gmail.com>
2023-03-02 21:37:38 +0700

    create script.js

---
+++ static/script.js
@@ -0,0 +1,22 @@
+messageBox = document.querySelector("#message-box")
+inputBox = document.querySelector("#input-box")
+form = document.querySelector("form")
+
+form.addEventListener("submit",async (e) => {
+    e.preventDefault();
+
+    const message = inputBox.value;
+
+    const response = await fetch("/completion", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            message: message,
+        }),
+    }).then((response) => response.json());
+
+    messageBox.innerHTML += `<li>Chatbot: ${response}</li>`;
+    inputBox.value = "";
+})
