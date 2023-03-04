ac2eb0e2424a8bbcdd2f432c78d358fab508f138
trnKhanh <tnk29082003@gmail.com>
2023-03-04 22:28:18 +0700

    redesign script.js to add chatbot type feature

--- static/script.js
+++ static/script.js
@@ -1,24 +1,26 @@
-messageBox = document.querySelector("#message-box")
-inputBox = document.querySelector("#input-box")
-form = document.querySelector("form")
+messageBox = document.querySelector(".message-box");
+inputBox = document.querySelector(".input-box");
+formSelect = document.querySelector(".form-select");
+form = document.querySelector("form");
 
 form.addEventListener("submit",async (e) => {
     e.preventDefault();
-
     const message = inputBox.value;
+    const chatbotType = formSelect.value;
 
-    messageBox.innerHTML += `<li>User: ${message}</li>`;
+    messageBox.innerHTML += `<div class="user-message">User: ${message}</div>`;
 
-    const response = await fetch("/completion", {
+    const response = await fetch(`/completion`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
             message: message,
+            type: chatbotType,
         }),
     }).then((response) => response.json());
 
-    messageBox.innerHTML += `<li>Chatbot: ${response}</li>`;
+    messageBox.innerHTML += `<div class="chatbot-message">Chatbot: ${response}</div>`;
     inputBox.value = "";
 })
